295ccd4668ebb61dd3c03b7d087223090780c22f
Andrew Cole <andrew.cole@illallangi.com>
2014-01-28 14:48:26 +1100

    Initial commit

---
+++ src/Illallangi.BEncode.Tests/BEncodeTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Illallangi.Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BEncodeTests
+    {
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(@"i3e", typeof(int)).Returns(3);
+                yield return new TestCaseData(@"i34e", typeof(int)).Returns(34);
+                yield return new TestCaseData(@"i345e", typeof(int)).Returns(345);
+
+                yield return new TestCaseData(@"4:spam", typeof(string)).Returns(@"spam");
+                yield return new TestCaseData(@"4:spamspam", typeof(string)).Returns(@"spam");
+                yield return new TestCaseData(@"12:spamspamspam", typeof(string)).Returns(@"spamspamspam");
+
+                yield return new TestCaseData(@"l4:spam4:eggse", typeof(List<dynamic>)).Returns(new List<dynamic> { "spam", "eggs" });
+
+                yield return new TestCaseData(@"d3:cow3:moo4:spam4:eggse", typeof(Dictionary<string, dynamic>))
+                                        .Returns(
+                                            new Dictionary<string, dynamic>
+                                                {
+                                                    { "cow", "moo" }, 
+                                                    { "spam", "eggs" },
+                                                });
+
+                yield return new TestCaseData(@"d4:spaml1:a1:bee'", typeof(Dictionary<string, dynamic>))
+                                        .Returns(
+                                            new Dictionary<string, dynamic>
+                                                {
+                                                    { "spam", new List<dynamic> { "a", "b" } },
+                                                });
+
+                yield return new TestCaseData(@"d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee", typeof(Dictionary<string, dynamic>))
+                                        .Returns(
+                                            new Dictionary<string, dynamic>
+                                                {
+                                                    { "publisher", "bob" },
+                                                    { "publisher-webpage", "www.example.com" },
+                                                    { "publisher.location", "home" },
+                                                });
+            }
+        }
+
+        [Test, TestCaseSource("TestCases")]
+        public dynamic CanRead(string text, Type t)
+        {
+            var result = BEncode.ReadText(text);
+            Assert.IsInstanceOfType(t, result);
+            return result;
+        }
+    }
+}
---
+++ src/Illallangi.BEncode/BEncode.cs
@@ -0,0 +1,129 @@
+﻿namespace Illallangi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public static class BEncode
+    {
+        public static dynamic ReadText(string text)
+        {
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(text)))
+            {
+                return BEncode.ReadStream(stream);
+            }
+        }
+
+        public static dynamic ReadFile(string filename)
+        {
+            using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                return BEncode.ReadStream(stream);
+            }
+        }
+
+        private static dynamic ReadStream(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream))
+            {
+                return BEncode.Read(reader);
+            }
+        }
+
+        private static dynamic Read(BinaryReader reader)
+        {
+            switch (reader.ReadChar())
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    return BEncode.ReadString(reader);
+                case 'i':
+                    return BEncode.ReadInt(reader);
+                case 'l':
+                    return BEncode.ReadList(reader);
+                case 'd':
+                    return BEncode.ReadDictionary(reader);
+                default:
+                    throw new FormatException();
+            }
+        }
+
+        private static string ReadString(BinaryReader reader)
+        {
+            var sb = new StringBuilder();
+            reader.BaseStream.Seek(-1, SeekOrigin.Current);
+            var length = BEncode.ReadInt(reader, ':');
+            
+            for (var i = 0; i < length; i++)
+            {
+                sb.Append(reader.ReadChar());
+            }
+
+            return sb.ToString();
+        }
+
+        private static int ReadInt(BinaryReader reader, char terminator = 'e')
+        {
+            var sb = new StringBuilder();
+            char? c = null;
+
+            do
+            {
+                if (c.HasValue)
+                {
+                    sb.Append(c.Value);
+                }
+
+                c = reader.ReadChar();
+            }
+            while (terminator != c);
+
+            return int.Parse(sb.ToString());
+        }
+
+        private static IList<dynamic> ReadList(BinaryReader reader, char terminator = 'e')
+        {
+            var result = new List<dynamic>();
+
+            if (terminator == reader.PeekChar())
+            {
+                return result;
+            }
+
+            do
+            {
+                result.Add(BEncode.Read(reader));
+            }
+            while (terminator != reader.PeekChar());
+
+            return result;
+        }
+
+        private static IDictionary<string, dynamic> ReadDictionary(BinaryReader reader, char terminator = 'e')
+        {
+            var result = new Dictionary<string, dynamic>();
+
+            if (terminator == reader.PeekChar())
+            {
+                return result;
+            }
+
+            do
+            {
+                result.Add(BEncode.Read(reader), BEncode.Read(reader));
+            }
+            while (terminator != reader.PeekChar());
+
+            return result;
+        }
+    }
+}
